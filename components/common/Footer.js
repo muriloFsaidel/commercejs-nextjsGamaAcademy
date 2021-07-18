b6d490fc2b83776de9f33f604faee71fa8cfe3c3
Murilo F. Saidel <51511259+muriloFsaidel@users.noreply.github.com>
2021-07-18 17:52:30 -0300

    Update Footer.js

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Footer = () => (
-  <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/507f3b1f6e243d616cb330356/e76cd57adeaf5c366ec82004f.js");</script>
   <footer className="pt-5">
     <div className="custom-container mb-5 pb-5 pt-5">
       <div className="row">
